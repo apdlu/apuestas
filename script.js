d33b527a6a22d3fdcf8b9adba0b75ef130c9d0c7
apdlu <73270277+apdlu@users.noreply.github.com>
2020-11-21 03:33:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function myFunction() {
+  var a = Math.floor(Math.random() * 5)+1;
+  var x, text;
+
+  // Get the value of the input field with id="numb"
+  x = document.getElementById("numb").value;
+
+  // If x is Not a Number or less than one or greater than 10
+  if (isNaN(x) || x < 1 || x > 5) {
+    text = "Número fuera de rango";
+
+  } else {
+    if (isNaN(x) || x == a ) {
+      text = "¡GANASTE! el número es: "+a;
+
+    } else {
+      text = "¡PERDISTE! el número es: "+a;
+
+    }
+  }
+  document.getElementById("demo").innerHTML = text;
+  
+}
+
